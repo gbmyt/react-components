d7a66f01a668886af902c352f4c85ff05e412644
gbmyt <gbmytabc1@gmail.com>
2022-10-16 16:49:46 -0400

    Refactored list items to individual React components

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // TODO
 const GroceryList = () => (
 	<ul>
-		<li>Item 1</li>
-		<li>Item 2</li>
+		<Milk />
+		<Eggs />
 	</ul>
+);
+
+const Milk = () => (
+	<li>Milk</li>
+);
+
+const Eggs = () => (
+	<li>Eggs</li>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
