d057d1eb95d81a4456ef2de5d4f8b12ac76172e2
gbmyt <gbmytabc1@gmail.com>
2022-10-16 17:44:51 -0400

    Make it so that when your mouse hovers over a <li> of a GroceryListItem that it turns bold

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const App = () => (
-	<div>
-		<h3>To Buy:</h3>
-		<GroceryList items={['Milk', 'Bread', 'Eggs', 'Flour', 'Paper Towels']}/>
-	</div>
-);
+// const { useState } = React;
+var useState = React.useState;
 
-const GroceryListItem = (props) => (
-	<li>{props.item}</li>
-);
+const GroceryListItem = (props) => {
+	const [isDone, setIsDone] = useState(false);
+
+	const style = {
+    fontWeight: isDone ? 'bold' : 'normal'
+  };
+
+  return (
+		<li style={style} onMouseEnter={() => setIsDone(!isDone)}>
+			{props.item}
+		</li>
+	);
+};
 
 const GroceryList = (props) => (
-	<ul>
-		{props.items.map((groceryItem) => (
-			<GroceryListItem item={groceryItem} />
-		))}
-	</ul>
+  <ul>
+  {props.items.map((groceryItem) => (
+      <GroceryListItem item={groceryItem} />
+  ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h3>To Buy:</h3>
+    <GroceryList items={['Milk', 'Bread', 'Eggs', 'Flour', 'Paper Towels']}/>
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById("app"));
